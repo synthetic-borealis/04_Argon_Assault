5572ef88d19ea0715c5325e46c4115a90d0ead7d
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-10-22 17:32:22 +0300

    AA05 Setup a splash screen

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("LoadFirstLevel", 3f);
+	}
+
+    void LoadFirstLevel ()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
