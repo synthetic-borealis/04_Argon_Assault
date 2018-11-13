49ce35148f7969e235621e6477837bc7ca238558
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-11-13 20:10:53 +0200

    AA19 SendMessage() between components

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,35 +4,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
-    [Tooltip("In ms^-1")] [SerializeField] float speed = 20f;
+    [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float controlSpeed = 20f;
     [Tooltip("In m")] [SerializeField] float xRange = 5f;
     [Tooltip("In m")] [SerializeField] float yRange = 3f;
 
+    [Header("Screen-position Based")]
     [SerializeField] float positionPitchFactor = -5f;
-    [SerializeField] float controlPitchFactor = -20f;
     [SerializeField] float positionYawFactor = 5f;
+
+    [Header("Control-throw Based")]
+    [SerializeField] float controlPitchFactor = -20f;
     [SerializeField] float controlRollFactor = -20f;
 
     float xThrow, yThrow;
-
-    // Use this for initialization
-    void Start ()
-    {
-		
-	}
-
-    void OnTriggerEnter(Collider other)
-    {
-        print("Player triggered something");
-    }
+    bool isControlEnabled = true;
 
     // Update is called once per frame
     void Update ()
     {
-        ProcessTranslation();
-        ProcessRotation();
+        if (isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
     }
 
     private void ProcessRotation()
@@ -50,17 +47,23 @@ public class Player : MonoBehaviour {
     private void ProcessTranslation()
     {
         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffsetThisFrame = xThrow * speed * Time.deltaTime;
+        float xOffsetThisFrame = xThrow * controlSpeed * Time.deltaTime;
 
         float rawXPos = transform.localPosition.x + xOffsetThisFrame;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
         yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        float yOffsetThisFrame = yThrow * speed * Time.deltaTime;
+        float yOffsetThisFrame = yThrow * controlSpeed * Time.deltaTime;
 
         float rawYPos = transform.localPosition.y + yOffsetThisFrame;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    void OnPlayerDeath() // called by string reference
+    {
+        isControlEnabled = false;
+        print("Player movement disabled");
     }
 }
