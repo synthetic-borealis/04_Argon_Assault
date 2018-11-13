d1ab1021f3f7b537d81c37564d3cfa22f30be18e
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-11-13 21:15:47 +0200

    AA20 Enable a GameObject from C#

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // ok to include as long as it's the only script loading scenes
 
 public class CollisionHandler : MonoBehaviour {
 
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
     void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
@@ -13,6 +16,15 @@ public class CollisionHandler : MonoBehaviour {
     private void StartDeathSequence()
     {
         print("Player died");
+        deathFX.SetActive(true);
         SendMessage("OnPlayerDeath");
+
+        Invoke("RestartScene", levelLoadDelay);
+    }
+
+    private void RestartScene() // string referenced
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
     }
 }
