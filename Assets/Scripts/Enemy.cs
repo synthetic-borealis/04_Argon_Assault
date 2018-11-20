5d3ec20872c7516702b0f175dca1f4589eabbfd6
Elhanan Flesch <elhanan.flesch@mail.huji.ac.il>
2018-11-21 00:01:23 +0200

    AA29 Enemy health system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : MonoBehaviour {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 12;
+    [SerializeField] int hits = 3;
 
     Scoreboard scoreBoard;
 
@@ -26,7 +27,22 @@ public class Enemy : MonoBehaviour {
 
     void OnParticleCollision(GameObject other)
     {
+        ProcessHits();
+        if (hits <= 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHits()
+    {
         scoreBoard.ScoreHit(scorePerHit);
+        hits--;
+        // TODO consider hit FX
+    }
+
+    private void KillEnemy()
+    {
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
